4286faf9f836acd3465c0144772370e5e9429f54
Akshat malik <akshatf8lmalik@gmail.com>
2026-03-02 22:57:42 +0530

    Added new feature : Resume pdf generator

--- backend/controller/resumeController.js
+++ backend/controller/resumeController.js
@@ -1,12 +1,12 @@
-const { Report } = require("../llm/structuredAnswer");
-const Resume = require("../schemas/resumeSchema");
+const { Report, Resume: generateResumePdf } = require("../llm/structuredAnswer");
+const ResumeModel = require("../schemas/resumeSchema");
 const pdfParse = require("pdf-parse");
 const generateReport = async(req,res) => {
     try {
         const resumeContent = await pdfParse(req.file.buffer);
         const {selfDescription,jobDescription} = req.body;
         const report = await Report(selfDescription, jobDescription, resumeContent.text);
-        const InterviewReport = await Resume.create({selfDescription,jobDescription,userId:req.user.id,resume:resumeContent.text,...report});
+        const InterviewReport = await ResumeModel.create({selfDescription,jobDescription,userId:req.user.id,resume:resumeContent.text,...report});
         res.status(200).json({InterviewReport});
     } catch (error) {
         console.log(error);
@@ -16,7 +16,7 @@ const generateReport = async(req,res) => {
 const getReportById = async(req,res) => {
     try {
         const {id} = req.params;
-        const report = await Resume.findOne({ _id : id , userId : req.user.id });
+        const report = await ResumeModel.findOne({ _id : id , userId : req.user.id });
         if(!report){
             return res.status(404).json({message:"Report not found"});
         }
@@ -29,7 +29,7 @@ const getReportById = async(req,res) => {
 }
 const allReports = async(req,res) => {
     try {
-        const reports = await Resume.find({userId : req.user.id}).sort({createdAt : -1}).select("AtsScore _id");
+        const reports = await ResumeModel.find({userId : req.user.id}).sort({createdAt : -1}).select("AtsScore _id");
         if(!reports){
             return res.status(404).json({message:"No reports found"});
         }
@@ -40,8 +40,29 @@ const allReports = async(req,res) => {
         res.status(500).json({message:"Internal server error"});
     }
 }
+const resumeGenerator = async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const report = await ResumeModel.findOne({ _id : id , userId : req.user.id }).select("selfDescription jobDescription resume");
+        if(!report){
+            return res.status(404).json({message:"Report not found"});
+        }
+        const pdfBuffer = await generateResumePdf({ resume: report.resume, selfDescription: report.selfDescription, jobDescription: report.jobDescription });
+        res.set({
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachment; filename=resume_${id}.pdf`
+    })
+
+    res.send(pdfBuffer);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
 module.exports = {
     generateReport,
     getReportById,
-    allReports
+    allReports,
+    resumeGenerator
 }
--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const {generateReport,getReportById,allReports} = require("../controller/resumeController");
+const {generateReport,getReportById,allReports,resumeGenerator} = require("../controller/resumeController");
 const upload = require("../middlewares/fileBuffer");
 const auth = require("../middlewares/auth");
 router.post("/generateReport",auth,upload.single("resume"),generateReport); 
 router.get("/getReportById/:id",auth,getReportById);
 router.get("/allReports",auth,allReports);
+router.get("/resumeGenerator/:id",auth,resumeGenerator);
 module.exports = router;
